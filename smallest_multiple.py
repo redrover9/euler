f2d661d6e27b10a7afb763cd4bd3d153e9390583
redrover9 <grace.thompson97@gmail.com>
2020-12-10 07:34:57 -0800

    Create smallest_multiple.py

---
+++ smallest_multiple.py
@@ -0,0 +1,8 @@
+smallest_multiple = 1
+
+while True:
+    if smallest_multiple % 1 == 0 and smallest_multiple % 2 == 0 and smallest_multiple % 3 == 0 and smallest_multiple % 4 == 0 and smallest_multiple % 5 == 0 and smallest_multiple % 6 == 0 and smallest_multiple % 7 == 0 and smallest_multiple % 8 == 0 and smallest_multiple % 9 == 0 and smallest_multiple % 10 == 0 and smallest_multiple % 11 == 0 and smallest_multiple % 12 == 0 and smallest_multiple % 13 == 0 and smallest_multiple % 14 == 0 and smallest_multiple % 15 == 0 and smallest_multiple % 16 == 0 and smallest_multiple % 17 == 0 and smallest_multiple % 18 == 0 and smallest_multiple % 19 == 0 and smallest_multiple % 20 == 0:
+        print(smallest_multiple)
+        break
+    else:
+        smallest_multiple += 1
